bcac20d0e7ebfd6540ae440976c4c85851616b97
nikhil-r-kumar07 <nikhil.r.kumar07@gmail.com>
2026-04-06 10:31:36 -0400

    Update fourbitalu.v

--- fourbitalu.v
+++ fourbitalu.v
@@ -1,4 +1,4 @@
-module 4bitalu (
+module fourbitalu (
     input [3:0] A, B,
     input [2:0] opcode,
     output [3:0] result,
@@ -7,3 +7,17 @@ module 4bitalu (
 );
 
 reg [3:0] result;
+reg carry;
+always @(*) begin
+    case (opcode) begin
+        3'b000 : A + B;
+        3'b001 : A - B;
+        3'b010 : A & B;
+        3'b011 : A | B;
+        3'b100 : A ^ B;
+        3'b101 : ~A;
+        3'b110 : A - 1;
+        3'b111 : A + 1;
+    end
+end
+endmodule
